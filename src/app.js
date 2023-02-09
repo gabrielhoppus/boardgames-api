c2178aed8c106daa075bbd99fc48361ac061b528
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-02-09 14:52:52 -0300

    patch: folder structure

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// import routers from "./routers/app.routers.js";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+// app.use(routers);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port: ${port}`))
---
+++ src/routers/index.routers.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+// import receitasRouters from "./receitas.routers.js";
+// import categoriasRouters from "./categorias.routers.js";
+
+const router = Router();
+
+router.use();
+router.use();
+
+
+
+export default router;
