b92d49d3c657c82852673cb0446e112f1e149a51
gabrielhoppus <luizgabriel.84@hotmail.com>
2023-02-09 21:11:39 -0300

    fix: insert code mistake + testing post and get data

--- src/controllers/games.controllers.js
+++ src/controllers/games.controllers.js
@@ -1,13 +1,11 @@
 import { db } from "../config/database.connection.js";
 
-export async function getGames(res) {
+export async function getGames(_,res) {
     try {
         const games = await db.query("SELECT * FROM games")
-
         res.send(games.rows)
-
     } catch (error) {
-        res.status(500).send(error.message)
+        res.sendStatus(500);
     }
 }
 
@@ -22,7 +20,7 @@ export async function postGames(req, res) {
             return res.status(409).send("Game already exists");
         }
 
-        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4); RETURNING *`,
+        await db.query(`INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4) RETURNING *;`,
             [name, image, stockTotal, pricePerDay])
         res.status(201).send("Game added successfully");
     } catch (error) {
